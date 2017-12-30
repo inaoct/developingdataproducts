9859e3e72c7cb14f3c2125e07ad004b7dde26dc0
inaoct <hristina.galabova@gmail.com>
2017-12-30 17:38:08 -0500

    add changes

--- SATScoreApp/server.R
+++ SATScoreApp/server.R
@@ -7,12 +7,9 @@
 #    http://shiny.rstudio.com/
 #
 
-
+## Handle library pre-requisites
 if(!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
 library(shiny)
-#library(shiny)
-
-## Handle library pre-requisites
 # Using dplyr for its more intuitive data frame processing
 if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
 library(dplyr)
@@ -22,14 +19,17 @@ library(ggmap)
 # Using leaflet for map-based visualizations
 if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
 library(leaflet)
+# Using scales for percentage formatting
+if(!require(scales)) install.packages("scales", repos = "http://cran.us.r-project.org")
+library(scales)
 
 collegeData <- read.table(
-    "~/developingdataproducts/FirstShinyApp/collegeData400.csv",
+    "./collegeData400.csv",
     sep = ",", header = TRUE, comment.char = "", quote = "\"")
 
 collegeMapData <-
     dplyr::select(collegeData, INSTNM, CITY, STABBR, INSTURL, SATMTMID,
-        SATVRMID, SATWRMID, lat, lng) %>%
+        SATVRMID, SATWRMID, C150_4_POOLED_SUPP, lat, lng) %>%
     dplyr::mutate(UnivName = as.character(INSTNM),
         City = as.character(CITY),
         State = as.character(STABBR),
@@ -38,9 +38,16 @@ collegeMapData <-
         # final html doc
         MedianMathSAT = suppressWarnings(as.numeric(as.character(SATMTMID))),
         MedianVerbalSAT = suppressWarnings(as.numeric(as.character(SATVRMID))),
-        MedianWritingSAT = suppressWarnings(as.numeric(as.character(SATWRMID)))
+        MedianWritingSAT = suppressWarnings(as.numeric(as.character(SATWRMID))),
+        CompletionRate = suppressWarnings(as.numeric(as.character(C150_4_POOLED_SUPP)))
     )
 
+lm.fitA <- lm(CompletionRate ~ MedianMathSAT, data = collegeMapData)
+
+lm.fitB <- lm(CompletionRate ~ MedianWritingSAT, data = collegeMapData)
+
+lm.fitC <- lm(CompletionRate ~ MedianMathSAT + 
+        MedianWritingSAT, data = collegeMapData)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -56,17 +63,30 @@ shinyServer(function(input, output) {
         nrow(filteredData)
     })
     
+    modelPredA <- reactive({
+        mathScoreInput <- input$sliderMATH
+        predict(lm.fitA, newdata = data.frame(MedianMathSAT = mathScoreInput))
+    })
+    
+    modelPredB <- reactive({
+        writingScoreInput <- input$sliderWRITING
+        predict(lm.fitB, newdata = data.frame(MedianWritingSAT = writingScoreInput))
+    })
+    
+    modelPredC <- reactive({
+        mathScoreInput <- input$sliderMATH
+        writingScoreInput <- input$sliderWRITING
+        predict(lm.fitC, newdata = data.frame(MedianMathSAT = mathScoreInput,
+            MedianWritingSAT = writingScoreInput))
+    })
+    
+   
+    
     output$plot1 <- renderLeaflet({
-        
-        
-        
         filteredSATData <- dplyr::filter(collegeMapData, MedianMathSAT > input$sliderMATH &
                 MedianVerbalSAT > input$sliderVERBAL &
                 MedianWritingSAT > input$sliderWRITING)
         
-        ##   highSATCollegeData <-
-        ##        dplyr::mutate(filteredSATData, lat = filteredSATData$lat, lng = filteredSATData$lng)
-        # univSATScore <- as.character(highSATCollegeData$MedianMathSAT)
         univLink <- paste("<a href=","'", "http://",
             filteredSATData$SiteURL, "'",
             " target='_blank'", #open link in a new tab
@@ -95,9 +115,38 @@ shinyServer(function(input, output) {
         
     })
     
+    output$plot2 <- renderPlot({
+        mathScoreInput <- input$sliderMATH
+        writingScoreInput <- input$sliderWRITING
+        par(mfrow=c(1,2))
+        
+        # model A vs model C
+        plot(collegeMapData$MedianMathSAT, collegeMapData$CompletionRate,
+            xlab = "Median Math SAT Score", ylab = "Completion Rate")
+        abline(lm.fitA, col = "red", lwd = 2)
+        abline(a = coef(lm.fitC)[1] + coef(lm.fitC)[3]*writingScoreInput, 
+            b = coef(lm.fitC)[2], col = "blue", lwd = 2)
+        legend(510, 0.2, c("Model A Prediction", "Model C Prediction"), pch = 16, 
+            col = c("red", "blue"), bty = "n", cex = 1.2)
+        points(mathScoreInput, modelPredA(), col = "red", pch = 16, cex = 2)
+        points(mathScoreInput, modelPredC(), col = "blue", pch = 16, cex = 2)
+        
+        # model B vs model C
+        plot(collegeMapData$MedianWritingSAT, collegeMapData$CompletionRate, 
+            xlab = "Median Writing SAT Score", ylab = "Completion Rate")
+        abline(lm.fitB, col = "green", lwd = 2)
+        abline(a = coef(lm.fitC)[1] + coef(lm.fitC)[2]*mathScoreInput, 
+            b = coef(lm.fitC)[3], col = "blue", lwd = 2)
+        legend(510, 0.2, c("Model B Prediction", "Model C Prediction"), pch = 16, 
+            col = c("green", "blue"), bty = "n", cex = 1.2)
+        points(writingScoreInput, modelPredB(), col = "green", pch = 16, cex = 2)
+        points(writingScoreInput, modelPredC(), col = "blue", pch = 16, cex = 2)
+        
+    })
     
+   
     output$scores <- renderText({
-        paste("Number of Universities with Median SAT Scores Higher Than: ", 
+        paste(
             "Math: ", input$sliderMATH, 
             "Verbal: ", input$sliderVERBAL, 
             "Writing: ", input$sliderWRITING)
@@ -106,5 +155,19 @@ shinyServer(function(input, output) {
     output$count1 <- renderText({
         univCount()
     })
+    
+    output$predA <- renderText({
+        percent(modelPredA())
+    })
+    
+    
+    output$predB <- renderText({
+        percent(modelPredB())
+    })
+    
+    output$predC <- renderText({
+        percent(modelPredC())
+    })
+    
     
 })
--- SATScoreApp/ui.R
+++ SATScoreApp/ui.R
@@ -17,7 +17,7 @@ library(leaflet)
 shinyUI(fluidPage(
     
     
-    titlePanel("Predict Horsepower from MPG"),
+    titlePanel("College Choices Based on SAT Scores"),
     sidebarLayout(
         sidebarPanel(
             sliderInput("sliderMATH", "What is Your Math SAT Score?", 400, 800, value = 500),
@@ -25,10 +25,28 @@ shinyUI(fluidPage(
             sliderInput("sliderWRITING", "What is Your Writing SAT Score?", 400, 800, value = 500)
         ),
         
-        mainPanel(
-            leafletOutput("plot1"),
-            h3(textOutput("scores")),
-            h4(textOutput("count1"))
-        )
-    )
-))
+        mainPanel(tabsetPanel(type = "tabs",
+            tabPanel("College Map", br(),
+                textOutput("out1"),
+                leafletOutput("plot1"),
+                h4("Total Number of Universities with Scores Greater Than"),
+                h4(textOutput("scores")),
+                h3(textOutput("count1"))),
+                
+            tabPanel("Prediction", br(),
+                plotOutput("plot2"),
+                h4("Predicted Completion Rate from Model A"),
+                h5("(Simple  Linear Regression on Math SAT)"),
+                textOutput("predA"),
+                h4("Predicted Completion Rate from Model B"),
+                h5("(Simple Linear Regression on Writing SAT)"),
+                textOutput("predB"),
+                h4("Predicted Completion Rate from Model C"),
+                h5("(Linear Regression on Math SAT and Writing SAT)"),
+                textOutput("predC")),
+          
+            tabPanel("Help", br(), 
+                includeHTML("./help.html"))
+        ))
+            
+)))
